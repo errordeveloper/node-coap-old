d261c5f763c793edd4363687948849766c4edc4f
Ilya Dmitrichenko <ilya.dmitrichenko@pachube.com>
2012-02-11 23:08:11 +0000

    Why the Olaf did `(0xFF & ...)` ?

--- test.js
+++ test.js
@@ -2,22 +2,29 @@ var dgram = require ('dgram');
 var inspect = require ('util').inspect;
 var server = dgram.createSocket ('udp6');
 
+COAP = { debugHook : function (object) { console.log (inspect (object)); } };
+
+COAP.OptionsTable = require ('./options');
+COAP.OptionsAgregate = require ('./agregate');
+
 
 var ParseBuffer = function (buffer) {
+
+  var Methods = [ , 'GET', 'POST', 'PUT', 'DELETE' ];
 
   var request = {
 
     // first byte: version, type, and option count
-    version:  (0xFF & ((buffer[0] & 0xC0) >>> 6)),
-    type:     (0xFF & ((buffer[0] & 0x30) >>> 4)),
+    version:  ((buffer[0] & 0xC0) >>> 6),
+    type:     ((buffer[0] & 0x30) >>> 4),
     options: {
-      count:  (0x0F & buffer[0]),
+      count:  (buffer[0] &0x0F),
       array: []
     },
     // second byte: method or response code
-    code:     (0xFF & buffer[1]),
+    code:     Methods[(buffer[1])],
     // third and forth byte: transaction ID (TID)
-    tid:      (buffer[2] << 8) | buffer[3],
+    tid:      ((buffer[2] << 8) | buffer[3]),
 
   };
   
@@ -30,7 +37,7 @@ var ParseBuffer = function (buffer) {
 
     var option = {
       start: 1,
-      type: (buffer[0] >>> 4), // ? (0xF0 & buffer[0] >>> 4) + delta 
+      type: (buffer[0] >>> 4),
       length: (buffer[0] & 0x0F)
 
     };
@@ -45,10 +52,12 @@ var ParseBuffer = function (buffer) {
       throw new Error("COAP option type is not elective!");
     }
     */
-    if (0 != (option.type % 14)) { // OPTION_TYPE_FENCEPOST = 14
+    //if (0 != (option.type % 14)) { // OPTION_TYPE_FENCEPOST = 14
       option.code = buffer.slice(option.start,option.end);
       request.options.array.push(option);
-    }
+      console.log(option.type+' => '+COAP.OptionsTable.getName(option.type));
+      //COAP.OptionsAgregate.append(option.type, option.code);
+    //}
     buffer = buffer.slice(option.end);
   }
 
