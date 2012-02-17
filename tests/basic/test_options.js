b446ec49ed87bbe0f41b0e37d5693f18307beaac
Ilya Dmitrichenko <ilya.dmitrichenko@pachube.com>
2012-02-17 00:39:15 +0000

    Tidy-up!

--- tests/basic/test_options.js
+++
@@ -1,7 +0,0 @@
-
-var COAP = { opt : require ('./options') };
-
-console.log(COAP.opt.getName(1)+' is of type `'+COAP.opt.dataType(1)+'`');
-console.log('isCritical: '+COAP.opt.isCritical(1));
-
-console.log('Content-Type: '+COAP.opt.byName['Content-Type']);
