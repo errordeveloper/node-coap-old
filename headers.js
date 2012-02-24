9d1421010b3faa952c8dc4b30d3c8b4038f2ac1f
Ilya Dmitrichenko <ilya.dmitrichenko@pachube.com>
2012-02-24 11:10:38 +0000

    Style & comments ...

--- headers.js
+++ headers.js
@@ -11,7 +11,7 @@ module.exports = (function () {
       connectionType:
         headers.decodeTypes[((buffer[0] & 0x30) >>> 4)],
       optionsCount:
-        (buffer[0] &0x0F),
+        (buffer[0] & 0x0F),
       // second byte: method or response code
       requestCode:
         headers.decodeMethods[(buffer[1])],
@@ -26,11 +26,14 @@ module.exports = (function () {
       // BEWARE:
       // The upper layer should provide us a consistent request.
       // * It _does not_ check for properties in the request!
+      // first byte: version, type, and option count
       request.payload[0] = 0x00;
       request.payload[0] |= (0x0F & (request.optionsCount));
       request.payload[0] |= (0xC0 & (request.protocolVersion << 6));
       request.payload[0] |= (0x30 & (request.connectionType << 4));
+      // second byte: method or response code
       request.payload[1] = headers.encodeMethods[request.requestCode];
+      // third and forth byte: transaction ID (TID)
       request.payload[2] = (0xFF & (request.transactionID >> 8));
       request.payload[3] = (0xFF & (request.transactionID));
 
