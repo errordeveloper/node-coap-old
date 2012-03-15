c66fbb42812eb557fec9037b736215c132460202
Ilya Dmitrichenko <ilya.dmitrichenko@pachube.com>
2012-03-15 01:15:23 +0000

    Improved the unit testing of `IntegerUtils`, still fails for size 4 ;(

--- tests/integer.unit.js
+++ tests/integer.unit.js
@@ -1,49 +1,60 @@
 #!/usr/bin/env node node_modules/nodeunit/bin/nodeunit
 var util = require ('util');
+
+function helper (test, u, x, n, k, str, max) {
+  //var n = 3; str = "one ocet", max = 0xFFFFFF;
+
+  function rp () {
+    return  Math.floor(Math.random()*0xFFFFFFFF);
+  }
+  function rn () {
+    return  -Math.floor(Math.random()*0xFFFFFFFF);
+  }
+
+  var R, Z;
+
+  u.write[n](x, k, max);
+  test.equal(u.read[n](x, k), max,
+      "Wrote "+max+" (maximum) as "+str+", should read the same!");
+
+  u.write[n](x, k, Z=rn());
+
+  test.equal(
+      R = u.read[n](x, k),
+      Z & max,
+      "Wrote "+Z+" (random), should read "+(Z&max)+" as "+str+"!");
+
+  test.notEqual(R, Z,
+      "Wrote "+Z+" (random), should not read "+Z+"!");
+
+  test.ok(
+      (R >= 0 && R <= max),
+      "Wrote "+Z+" (random), should read value between 0 and "+max+"!");
+
+  u.write[n](x, k, Z=rp());
+
+  test.equal(
+      R = u.read[n](x, k),
+      Z & max,
+      "Wrote "+Z+" (random), should read "+(Z&max)+" as "+str+"!");
+
+  test.notEqual(R, Z,
+      "Wrote "+Z+" (random), should not read "+Z+"!");
+
+  test.ok(
+      (R >= 0 && R <= max),
+      "Wrote "+Z+" (random), should read value between 0 and "+max+"!");
+}
+
 exports['Equivalence mapping'] = function (test) {
+
   var u = require('../integer');
   var x = new Buffer(200000000+64);
 
-  /*
-  #include <stdint.h>
-  #include <stdio.h>
-  int main () {
-    uint8_t x8 = -1;
-    uint16_t x16 = -1;
-    uint32_t x32 = -1;
-    printf("var max8 = %u;\n", x8);
-    printf("var max16 = %u;\n", x16);
-    printf("var max32 = %u;\n", x32);
-  }
-  */
-  var max8 = 255;
-  var max16 = 65535;
-  var max32 = 4294967295;
-  var max24 = max32+max16+max8; // or something like that :)
-
-  for (var k = 0; k > 200000000; k++) {
-
-    u.write[1](x, k, max8);
-    test.equal(u.read[1](x, k), max8);
-    u.write[1](x, k, max8-(k%max8));
-    test.equal(u.read[1](x, k), max8-(k%max8));
-
-    u.write[2](x, k, max16);
-    test.equal(u.read[2](x, k), max16);
-    u.write[2](x, k, max16-(k%max16));
-    test.equal(u.read[2](x, k), max16-(k%max16));
-
-    u.write[3](x, k, max24);
-    test.equal(u.read[3](x, k), max24);
-    u.write[3](x, k, max24-(k%max24));
-    test.equal(u.read[3](x, k), max24-(k%max24));
-
-    u.write[4](x, k, max32);
-    test.equal(u.read[4](x, k), max32);
-    u.write[4](x, k, max32-(k%max32));
-    test.equal(u.read[4](x, k), max32-(k%max32));
-
-  }
+  helper(test, u, x, 1, 100, "one ocet", 0xFF);
+  helper(test, u, x, 2, 100, "two ocets", 0xFFFF);
+  helper(test, u, x, 3, 100, "three ocets", 0xFFFFFF);
+  helper(test, u, x, 4, 100, "four ocets", 0xFFFFFFFF);
 
   test.done();
 
