a2270b225568bc093ac526e7b434f33974483c0c
Ilya Dmitrichenko <ilya.dmitrichenko@pachube.com>
2012-03-15 01:33:54 +0000

    Added test agains core library and found something interesting!

--- tests/integer.unit.js
+++ tests/integer.unit.js
@@ -17,6 +17,23 @@ function helper (test, u, x, n, k, str, max) {
   test.equal(u.read[n](x, k), max,
       "Wrote "+max+" (maximum) as "+str+", should read the same!");
 
+  //TODO: annotate these and do `x.write*()` as well!
+  if (n === 1) {
+    test.equal(u.read[n](x, k), x.readUInt8(k));
+  }
+  if (n === 2) {
+    //DANGER:  this code seems to break Node (or nodeunit?)!
+    // `x.readUInt16(k));`
+    // it gets dead slow and after few second pause it throws!
+    test.equal(u.read[n](x, k), x.readUInt16BE(k));
+  }
+  //if (n === 3) {
+  //  test.throws(u.read[n](x, k), x.readUInt24BE(k));
+  //}
+  if (n === 4) {
+    test.equal(u.read[n](x, k), x.readUInt32BE(k));
+  }
+
   u.write[n](x, k, Z=rn());
 
   test.equal(
@@ -54,7 +71,7 @@ exports['Equivalence mapping'] = function (test) {
   helper(test, u, x, 1, 100, "one ocet", 0xFF);
   helper(test, u, x, 2, 100, "two ocets", 0xFFFF);
   helper(test, u, x, 3, 100, "three ocets", 0xFFFFFF);
-  //helper(test, u, x, 4, 100, "four ocets", 0xFFFFFFFF);
+  //FIXME: helper(test, u, x, 4, 100, "four ocets", 0xFFFFFFFF);
 
   test.done();
 
