9b4f7e45c232dcf260e5efb228f39a6298bed00a
Ilya Dmitrichenko <ilya.dmitrichenko@pachube.com>
2012-05-13 20:25:31 +0100

    Ditch complex helpers

--- helpers.js
+++ helpers.js
@@ -1,45 +1,12 @@
-module.exports = ( function Helpers (stack, hooks, dgram) {
-
-  /* This is very rought, needs some major re-work! */
+module.exports = ( function Helpers () {
 
   var helpers = {
-    createSocket: function (type) {
-      var socket = dgram.createSocket(type);
+    uri: function MakeURI (path) {
+      // TODO: make it somewhat similar to `uri` module
       return {
-        send: function Sender (message, callback) {
-          stack.ParseMessage.encode(message, function (buffer) {
-            with (message) {
-              socket.send(payload, 0, payload.length, origin.port, origin.address, callback);
-            }
-          });
-        }
+       'Uri-Path': path.split('/')
       };
-    },
-    // parse method/response codes and uri into internal representation
-    newMessage: function (code, uri) {
-      var message = {
-        //origin: {address: '::1', port: 5683},
-        protocolVersion: 1,
-        //messageType: 'CON',
-        //messageID: 2400239,
-        messageCode: 0,
-        options: { }
-      };
-      var methods = [ undefined, 'GET', 'POST', 'PUT', 'DELETE' ]
-      switch (typeof code) {
-        case 'number':
-          message.code = code;
-          break;
-        case 'string':
-          if (methods.indexOf(code) >= -1) {
-            message.code = methods.indexOf(code);
-            break;
-          } // otherwise fall-through and throw
-        default:
-          throw new Error('Wrong argument type for `code`!');
-      }
-      return message;
     }
   };
 
-  return { createSocket: helpers.createSocket, newMessage: helpers.newMessage }; } );
+  return { MakeURI: helpers.uri }; } () );
