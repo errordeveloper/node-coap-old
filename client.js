0c9ad69b4bddbff85048f087f5d2e14bf79bc947
Ilya Dmitrichenko <ilya.dmitrichenko@pachube.com>
2012-05-14 01:00:31 +0100

    Misc improvement of basic client example

--- client.js
+++ client.js
@@ -15,13 +15,23 @@ var statsHook = function (ref, inc) {
   console.log("stats["+ref+"]= "+stats[ref]);
 };
 
-var COAP = require ('./')({debug: debugHook, stats: statsHook});
+if(process.env['DEBUG']) {
+  var COAP = require ('./')({debug: debugHook, stats: statsHook});
+} else {
+  var COAP = require ('./')({});
+}
 
-COAP.request({
-  method: 'GET',
-  host: '::1',
-  path: '.well-known/core'
-}, function (rx) {
-  debugHook(rx);
-  console.log(rx.payload.toString());
-});
+var endpoints = ['.well-known/core', 'time', '/'];
+
+for (var i in endpoints) {
+  COAP.request({
+    method: 'GET',
+    host: '::1',
+    path: endpoints[i]
+  }, function (rx) {
+    //console.log(this);
+    console.log(rx.payload.toString());
+  });
+}
+
+setTimeout(process.exit, 1000);
